c58069da94fc2757a5dbabcef2a04d58e1a3fc34
Angelica <Angelica137@users.noreply.github.com>
2020-04-13 20:08:44 +0100

    post peep unit test, fail

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -2,7 +2,7 @@ feature 'Post peep' do
   scenario 'A user wants to post to Chitter' do
     visit('/')
     fill_in('text', with: 'My first peep')
-    click_button('Post')
+    click_button('Post') 
 
     expect(page).to have_content 'My first peep'
   end
